ff0f88311a955831283d4c95cb9dadc35b3c5263
Dhruv Patel <dhruv.m.patel@ecompliance.com>
2021-01-07 13:57:10 -0600

    chore: update test

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App'
 
 test('renders learn react link', () => {
     render(<App />)
-    const linkElement = screen.getByText(/learn react/i)
+    const linkElement = screen.getByText(/Welcome to react-typescript-starter-app/i)
     expect(linkElement).toBeInTheDocument()
 })
