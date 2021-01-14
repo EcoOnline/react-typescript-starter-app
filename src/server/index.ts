b60b8024da0c0c433d9db09766a684b8dd1beadc
Dhruv Patel <dhruv.m.patel@ecompliance.com>
2021-01-14 17:43:15 -0600

    fix: allow server to process requests to endpoints before hitting catch-all route

--- src/server/index.ts
+++ src/server/index.ts
@@ -9,12 +9,12 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(express.static(buildDir))
 
-app.get('/*', function (req, res) {
-    res.sendFile(path.join(buildDir, 'index.html'))
-})
-
 app.get('/home', (req: express.Request, res: express.Response) => {
     res.json({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' })
+})
+
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(buildDir, 'index.html'))
 })
 
 app.listen(port, () => {
