b6911a6c20198fade259d6f018e6ffc28b8bdd42
Dhruv Patel <dhruv.m.patel@ecompliance.com>
2021-01-25 09:55:54 -0600

    test: add test for GET /health endpoint

--- src/server/app.test.ts
+++ src/server/app.test.ts
@@ -2,6 +2,16 @@ import request from 'supertest'
 import app from './app'
 
 describe('Integration tests: App', () => {
+    test('GET /health should return 200 response with body', (done) => {
+        request(app)
+            .get('/health')
+            .end((err, res) => {
+                expect(res.status).toBe(200)
+                expect(res.body.status).toEqual('healthy')
+                done()
+            })
+    })
+
     test('GET /home should return 200 response with body', (done) => {
         request(app)
             .get('/home')
